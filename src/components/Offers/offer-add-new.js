764c505d94e0fde1e15951677c12123e271a6a52
raksha <pacholebhoomi1998@gmail.com>
2023-04-25 12:14:39 +0530

    add offer

--- src/components/Offers/offer-add-new.js
+++ src/components/Offers/offer-add-new.js
@@ -2,112 +2,146 @@ import React, { useState, useEffect } from "react";
 import Navbar from '../../directives/navbar'
 import Sidebar from '../../directives/sidebar'
 import Footer from "../../directives/footer";
-import { Button } from "bootstrap";
+import { Button, Input } from "reactstrap";
+import axios from "axios";
+import toast, { Toaster } from 'react-hot-toast'
+// import {toast} from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
 
 function Offeraddnew() {
-  const [user, setUser] = useState([]);
+  const [store, setStores] = useState([]);
   const [name, setName] = useState("");
-
-  const [selectValue, setSelectValue] = useState([]);
-  const [isChecked, setIsChecked] = useState(false);
- 
-  
-  const [messsge, setMessage] = useState('');
   const [description, setDescription] = useState('');
- 
+  const [fileData, setFileData] = useState([]);
+  const [selectValue, setSelectValue] = useState([]);
+  const [coupon_value, SetCoupon_value] = useState("");
+  const [coupon_code, SetCoupon_code] = useState("");
+  const [coupon_type, setCoupon_type] = useState("")
+  const [isChecked, setIsChecked] = useState(false);
+  const [datebegin, setDatebegin] = useState("")
+  const [dateend, setDateEnd] = useState("")
+  // const [couponConfig, setCouponCode] = useState([{coupon_type:""},{value:""},{coupon_code:""}])
+  const [message, setMessage] = useState("")
+  const [user, setUser] = useState([]);
 
-  const [makeAdeal, setMakeAdeal] = useState([{ datebegin: "", dateend: "", },]);
-  const [coupon, setCoupon] = useState([{ coupon_code: "", value: "", coupon_type:""},]);
-  const [fileData, setFileData] = useState();
 
-  const handleFilechange = (e) => {
-    setFileData(e.target.files[0]);
+
+
+  const onFileChange = (event) => {
+    setFileData(event.target.files[0]);
+    console.log(event.target.files[0])
+  }
+  const onChangeHandle = (event) => {
+    setSelectValue(event.target.value);
+  }
+  const onChangeHandleCoupon = (e) => {
+    setCoupon_type(e.target.value);
   };
 
-  const onChangeMakeAdeal = (e,name) => {
-    setMakeAdeal({ [name]: e.target.value });
-    console.log("makeAdeal",makeAdeal);
-  };
-
-  const onChangeHandleCoupon = (e,name) => {
-    setCoupon({ [name]: e.target.value });
-    console.log("setCoupon",setCoupon);
+  const onChangeHandleCouponCode = (event) => {
+    SetCoupon_code(event.target.value);
   };
 
   const handleMessageChange = (event) => {
     setDescription(event.target.value);
-    console.log(event.target.value);
+    // console.log(event.target.value);
+  };
+  const onChangeHandleCouponValue = (event) => {
+    SetCoupon_value(event.target.value);
   };
 
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
+    // setIsChecked(true);
   };
-
 
   const allStore = () => {
     fetch(`https://nearbyplaceadminpanner.onrender.com/api/v1/allstores`)
       .then((res) => res.json())
       .then((responsive) => {
-        console.log("tsaryhxdashgxfahsxasx", responsive.stores);
+      
         setUser(responsive.stores)
       })
       .catch((error) => {
-        console.log("error", error);
+        // console.log("error", error);
       })
   }
-
-  const onChangeHandle = (event) => {
-    const value = event.target.value;
-    setSelectValue(value);
-    console.log(value);
-  }
+// useEffect(()=>{
+//   allStore();
+// },[user])
 
   let handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      let res = await fetch("https://nearbyplaceadminpanner.onrender.com/api/v1/createnewoffer", {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          description: description,
-           PricingOfferValue: selectValue,
-          couponConfig: coupon,
-          makeAdeal: makeAdeal,
-          file: fileData
-        }),
+    var formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('file', fileData);
+    formData.append('PricingOfferValue', selectValue);
+    formData.append('coupon_type',coupon_type);
+    formData.append('value',coupon_value);
+    formData.append('coupon_code',coupon_code);
+    formData.append('datebegin',datebegin);
+    formData.append('dateend',dateend);
+    axios({
+      method: "post",
+      url: "https://nearbyplaceadminpanner.onrender.com/api/v1/createnewoffer",
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(response => {
+        setMessage(response.data.message);
+          toast.success("offer created successfully");
+       
+          setTimeout(() => {
+            handleMeaage();
+        }, 300);
+      })
+      .catch(error => {
+        console.log(error);
       });
-      let resJson = await res.json();
-      setMessage("Offer created successfully");
-      }
-    catch (err) {
-      console.log(err);
-    }
   };
 
-  useEffect(() => {
-    allStore();
-  }, [user])
+const handleMeaage = ()=>{
+  setCoupon_type("");
+  setName("");
+  setSelectValue([]);
+  setFileData([]);
+  SetCoupon_value("")
+  // setStores("");
+  SetCoupon_code("");
+  setSelectValue("");
+  setDateEnd("");
+  setDatebegin("");
+  setDescription("");
+
+}
 
   return (
     <>
+    <Toaster/>
       <Navbar />
       <Sidebar />
       <div className='content-wrapper'>
         <div className="main-panel-content">
           <div className='section-panel'>
             <div className='container'>
+              <form onSubmit={handleSubmit}>
+                <div className='row'>
+                  <div className='col-lg-6'>
+                    <div className="product-list-box">
+                      <div className="product-list-box-header">
+                        <h3><b>Add new offer</b></h3>
+                         {/* {message && ( <span className="text-success">{message}</span>)} */}
+                       
+                      </div>
+                      <div className="product-card-body">
 
-              <div className='row'>
-                <div className='col-lg-6'>
-                  <div className="product-list-box">
-                    <div className="product-list-box-header">
-                      <h3><b>Add new offer</b></h3>
-                    </div>
-                    <div className="product-card-body">
-                      <form onSubmit={handleSubmit}>
                         <div className="form-group">
                           <label>Store</label>
-                          <select id="inputState" className="form-control">
+                          <select id="inputState" className="form-control"
+                            value={store}
+                            onChange={(e) => setStores(e.target.value)}>
                             <option selected>Choose...</option>
                             {user.map((item, index) => {
                               return <option key={index}>{item.name}</option>;
@@ -118,9 +152,10 @@ function Offeraddnew() {
                         <div className="form-group">
 
                           <label>Name</label>
-                          <input type="text" className="form-control"
-                            value={name} onChange={(e) => setName(e.target.value)} 
+                          <Input type="text" className="form-control"
+                            value={name} onChange={(e) => setName(e.target.value)}
                             placeholder="Enter..." />
+
                         </div>
                         <div className="form-group">
                           <label>Description</label>
@@ -132,26 +167,25 @@ function Offeraddnew() {
                         </div>
                         <div className="form-group image-size">
                           <label>Images</label>
-                          <input type="file"
-                            className="form-control" placeholder="Enter..."
-                            value={fileData}
-                            onChange={(e) => handleFilechange(e)} />
+                          <input type="file" onChange={onFileChange}
+                            className="form-control" placeholder="Enter..." />
                         </div>
-                      </form>
+
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className='col-lg-6'>
-                  <div className='row'>
-                    <div className='col-sm-12'>
-                      <div className="product-list-box">
-                        <div className="product-list-box-header">
-                          <h3><b>Pricing & Offer value</b></h3>
-                        </div>
-                        <div className="product-card-body">
-                          <form>
+                  <div className='col-lg-6'>
+                    <div className='row'>
+                      <div className='col-sm-12'>
+                        <div className="product-list-box">
+                          <div className="product-list-box-header">
+                            <h3><b>Pricing & Offer value</b></h3>
+                          </div>
+                          <div className="product-card-body">
                             <div className="form-group">
-                              <select id="inputState" onChange={onChangeHandle} className="form-control">
+                              <select id="inputState" onChange={onChangeHandle}
+                                className="form-control"
+                                value={selectValue}>
                                 <option selected>-- Select Type</option>
                                 <option value="100">100</option>
                                 <option value="1100">1100</option>
@@ -161,105 +195,98 @@ function Offeraddnew() {
                                 <option value="12000">12000</option>
                                 <option value="5000">50000</option>
                                 <option value="100000">1,00000</option>
-
                               </select>
                             </div>
-                          </form>
+                          </div>
                         </div>
                       </div>
-                    </div>
 
-                    <div className='col-sm-12'>
-                      <div className="product-list-box">
-                        <div className="product-list-box-header">
-                          <h3><b>Coupon Config</b></h3>
-                        </div>
-                        <div className="product-card-body">
-                          <form>
+                      <div className='col-sm-12'>
+                        <div className="product-list-box">
+                          <div className="product-list-box-header">
+                            <h3><b>Coupon Config</b></h3>
+                          </div>
+                          <div className="product-card-body">
                             <div className="form-row">
                               <div className="form-group col-md-6">
                                 <label>Coupon type</label>
-                                <select id="inputState" 
-                                 onChange={(e) => onChangeHandleCoupon("coupon_type",e.target.value)} className="form-control">
-                                  <option selected>disabled</option>
+                                <Input type="text" className="form-control" placeholder='Enter...'
+                                  value={coupon_type}
+                                  onChange={(e) => onChangeHandleCoupon(e)} />
+                                {/* <select id="inputState"
+                                  onChange={(e) => onChangeHandleCoupon(e)}
+                                  className="form-control"
+                                  value={coupon_type}>
                                   <option value="enabled">enabled</option>
                                   <option value="disabled">disabled</option>
-                                </select>
+                                </select> */}
                               </div>
                               <div className="form-group col-md-6">
                                 <label>Value</label>
-                                <input type="number" className="form-control" placeholder='Enter...'
-                                  value={coupon.coupon_value}
-                                  onChange={(e) => onChangeHandleCoupon("value",e.target.value)} />
+                                <Input type="number" className="form-control" placeholder='Enter...'
+                                  value={coupon_value}
+                                  onChange={(e) => onChangeHandleCouponValue(e)} />
                               </div>
                               <div className="form-group col-md-12">
                                 <label>Coupon code</label>
-                                <input type="number" className="form-control" placeholder='Enter...'
-                                  value={coupon.coupon_code}
-                                  onChange={(e)=> onChangeHandleCoupon("coupon_code",e.target.value)}
+                                <input type="text" className="form-control" placeholder='Enter...'
+                                  value={coupon_code}
+                                  onChange={(e) => onChangeHandleCouponCode(e)}
                                 />
                               </div>
                             </div>
-
                             <p className="text-blue">This feature allows users to acquire and save coupons within the app. If you set a limited coupon, the value will decrease after each redemption.To view who has redeemed coupons from offers, go to 'Manage coupons'.</p>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='col-sm-12'>
-                      <div className="product-list-box">
-                        <div className="product-list-box-header">
-                          <h3><b>Deal Option</b></h3>
-                        </div>
-                        <div className="product-card-body">
-                          <div className="form-group">
-                            <input type="checkbox" checked={isChecked}
-                              onChange={handleCheckboxChange} className='all-create-checkbox' />
-                            <label> Make as a deal</label>
                           </div>
-                          {isChecked &&
-                            < div className='row mt-3' >
-                              <div className="col-lg-12">
-                                <div className='row'>
-                                  <div className='col-sm-6'>
-                                    <div className="form-group">
-                                      <label>Date Begin</label>
-                                      <input type="date" className="form-control"
-                                        value={makeAdeal.datebegin}
-                                      
-                                        onChange={(e) =>
-                                          onChangeMakeAdeal("datebegin", e.target.value)}
-                                        
-                                       
-                                      />
-                                    </div>
-                                  </div>
-                                  <div className='col-sm-6'>
-                                    <div className="form-group">
-                                      <label>Date End</label>
-                                      <input type="date" className="form-control"
-                                        value={makeAdeal.dateend}
-                                        onChange={(e) =>
-                                          onChangeMakeAdeal("dateend",e.target.value)}
-                                    
-                                      />
-                                    </div>
-                                  </div>
-                                </div>
-                                <div className='user-head'>
-                                  <Button><i className="fa fa-check-square-o" /> Create</Button>
-                                </div>
-                              </div>
-                            </div >
-                          }
                         </div>
                       </div>
+                      <div className='col-sm-12'>
+                        <div className="product-list-box">
+                          <div className="product-list-box-header">
+                            <h3><b>Deal Option</b></h3>
+                          </div>
+                          <div className="product-card-body">
+                            <div className="form-group">
+                              <input type="checkbox" checked={isChecked}
+                                onChange={handleCheckboxChange} className='all-create-checkbox' />
+                              <label> Make as a deal</label>
+                            </div>
+                            {isChecked &&
+                              < div className='row mt-3' >
+                                <div className="col-lg-12">
+                                  <div className='row'>
+                                    <div className='col-sm-6'>
+                                      <div className="form-group">
+                                        <label>Date Begin</label>
+                                        <Input type="date" className="form-control"
+                                          value={datebegin}
+                                          onChange={(e) => setDatebegin(e.target.value)} />
+                                      </div>
+                                    </div>
+                                    <div className='col-sm-6'>
+                                      <div className="form-group">
+                                        <label>Date End</label>
+                                        <input type="date" className="form-control"
+                                          value={dateend}
+                                          onChange={(e) => setDateEnd(e.target.value)} />
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='user-head'>
+                                    <Button type="submit"><i className="fa fa-check-square-o" /> Create</Button>
+                                  </div>
+                                </div>
+                              </div >
+                            }
+                          </div>
+                        </div>
+                      </div>
+
                     </div>
                   </div>
                 </div>
-              </div>
+              </form>
             </div>
+
           </div>
         </div>
       </div>
@@ -269,4 +296,4 @@ function Offeraddnew() {
   )
 }
 
-export default Offeraddnew
+export default Offeraddnew;
